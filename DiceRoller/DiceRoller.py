8c5d5cbefc12cfff5f4c53b94ceaa7902a7e85a4
Annabelle <18199611+ann4belle@users.noreply.github.com>
2021-10-02 19:57:56 -0500

    hatred

--- DiceRoller/DiceRoller.py
+++ DiceRoller/DiceRoller.py
@@ -12,12 +12,12 @@ class DiceRoller(commands.Cog):
 
     @commands.command()
     async def roll(self, ctx):
-        print(ctx.message.content)
+        await ctx.reply(ctx.message.content)
         if(ctx.message == ''):
             await ctx.reply('For syntax, see: <https://www.kreativekorp.com/dX/>')
             return
         r = requests.get(url = API, params = {'roll':ctx.message, 'format':'json', 'optimize':'false', 'evaluate':'true', 'expound':'true'})
-        print(r.text)
+        await ctx.reply(r.text)
         if(r.status_code != 200):
             await ctx.reply('Something went wrong - please try again later.')
             await RedBase.send_to_owners(ctx.message + '\nHTTP ' + r.status_code + '\nResponse: ' + r.raw)
